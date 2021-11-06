5284a4d552275e5c194602b11873abb0b7728cc7
DESKTOP-GVNOMUQ\tangt <tangtientuan89@gmail.com>
2021-11-06 11:30:24 +0700

    update

--- register.js
+++ register.js
@@ -1,6 +1,7 @@
 document.getElementById('register-button').addEventListener('click', submitForm)
 
 function submitForm() {
+  const urlGGForm = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScEIFthyMLA-EAcZ2-Gm0ma8jAfWtbo-a0uVTGDiIn1E91jUw/formResponse'
   const name = document.getElementById('name').value
   const phone = document.getElementById('phone').value
   const email = document.getElementById('email').value
@@ -18,7 +19,7 @@ function submitForm() {
 
   // * use request post with formData:
   $.ajax({
-    url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScEIFthyMLA-EAcZ2-Gm0ma8jAfWtbo-a0uVTGDiIn1E91jUw/formResponse',
+    url: urlGGForm,
     data: formData,
     processData: false,
     contentType: false,
