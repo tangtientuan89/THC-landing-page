2cd1a349467a674fff611ee7641f7ab1582fd443
DESKTOP-GVNOMUQ\tangt <tangtientuan89@gmail.com>
2021-11-06 09:52:02 +0700

    update

--- register.js
+++ register.js
@@ -1,1 +1,24 @@
-console.log('hihihihihhihihihihih')
+
+document.getElementById('register-button').addEventListener('onclick', submitForm())
+
+
+function submitForm() {
+  console.log('11111111111111111111')
+  const name = document.getElementById('name')
+  const phone = document.getElementById('phone')
+  const email = document.getElementById('email')
+
+  var fd = new FormData();
+  fd.append('file', input.files[0]);
+
+  $.ajax({
+    url: 'http://example.com/script.php',
+    data: fd,
+    processData: false,
+    contentType: false,
+    type: 'POST',
+    success: function (data) {
+      alert(data);
+    }
+  });
+}
