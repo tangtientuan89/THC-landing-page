ca4bf1a728b2ba95859eff947ad838bfc9ee4b82
DESKTOP-GVNOMUQ\tangt <tangtientuan89@gmail.com>
2021-11-07 07:57:33 +0700

    update

--- register.js
+++ register.js
@@ -1,21 +1,21 @@
 document.getElementById('register-button').addEventListener('click', submitForm)
 
 function submitForm() {
-  const urlGGForm = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScEIFthyMLA-EAcZ2-Gm0ma8jAfWtbo-a0uVTGDiIn1E91jUw/formResponse'
+  const urlGGForm = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdey39l1a8wDE-xzf4rZIAgpf8ho5vTqVoFzctaYhSsTiKtyg/formResponse'
   const name = document.getElementById('name').value
   const phone = document.getElementById('phone').value
   const email = document.getElementById('email').value
 
   // * handle submit form GG => from api submit (F12), define all key in Obj of formData: 
   let formData = new FormData();
-  formData.append('entry.675137429', name);
-  formData.append('entry.442234116', phone);
-  formData.append('entry.403426239', email);
-  formData.append('dlut', 1636171753101);
+  formData.append('entry.395664294', name);
+  formData.append('entry.1571854677', phone);
+  formData.append('entry.909301835', email);
+  formData.append('dlut', 1636246569394);
   formData.append('fvv', 1);
-  formData.append('partialResponse', [null, null, "-8687084246478428735"]);
+  formData.append('partialResponse', [null, null, "8328452313256165698"]);
   formData.append('pageHistory', 0);
-  formData.append('fbzx', -8687084246478428735);
+  formData.append('fbzx', 8328452313256165698);
 
   // * use request post with formData:
   $.ajax({
